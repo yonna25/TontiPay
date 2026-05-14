2baf4aae395643fc0aea2ba9cea56ba00853d32a
yonna25 <yonnadigit@gmail.com>
2026-05-14 13:42:12 +0100

    Remove server host configuration
    Removed server configuration for local network testing.

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server: {
-    // Utile pour tester sur mobile via votre réseau local
-    host: true 
-  }
 })
